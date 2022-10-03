0654d22e1745ef3bc7b6fd7dfdb7f4ad7e40b434
ryan howe <ryan.wm.howe@gmail.com>
2022-10-03 19:55:37 -0400

    added-clicking-button

--- learning-session-3-react/src/App.tsx
+++ learning-session-3-react/src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import nerLogo from './NER-Logo-App-Icon.svg';
+import logo from './logo.svg'
 
 function App() {
+
+  const [clicks, setClicks] = useState(0);
+  const [original, setOriginal] = useState(false);
+
+  const countClicks = () => {
+    setClicks(clicks + 1);
+    setOriginal(!original)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={nerLogo} className="App-logo" alt="logo" />
+        <h1>Ryan Howe</h1>
+        <h1>{clicks}</h1>
+        {original ? <img src={logo} className="App-logo" alt="logo" /> : <img src={nerLogo} className="App-logo" alt="logo" />}
         <p>NER React Learning Session</p>
         <a
           className="App-link"
@@ -17,6 +29,7 @@ function App() {
           Check Out Finishline!
         </a>
       </header>
+      <button onClick={countClicks}>add a click</button>
     </div>
   );
 }
