49fee8682c2c10842ae042dac1d2d54499875fc3
Jared Lyon <jaredalyon@gmail.com>
2022-10-03 19:48:42 -0400

    Add name + button clicker counter

--- learning-session-3-react/src/App.tsx
+++ learning-session-3-react/src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import nerLogo from './NER-Logo-App-Icon.svg';
 
 function App() {
+  const [clicker, setClickerCounter] = useState(0);
+
+  const onSwitchClicked = () => {
+    setClickerCounter(clicker + 1);
+  }
+
   return (
     <div className="App">
+      <header>
+        Jared Lyon's Branch
+      </header>
       <header className="App-header">
         <img src={nerLogo} className="App-logo" alt="logo" />
         <p>NER React Learning Session</p>
@@ -16,6 +25,10 @@ function App() {
         >
           Check Out Finishline!
         </a>
+        <button onClick={onSwitchClicked} className={"Clicker-Counter"}>
+          Clicker Counter!
+        </button>
+        <p> { clicker } </p>
       </header>
     </div>
   );
