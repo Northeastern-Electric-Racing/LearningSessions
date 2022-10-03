07551bc7826fddf61514dcba901a871aeeb2f0f4
“airman416” <im.armaan.agrawal@gmail.com>
2022-10-03 19:48:32 -0400

    Added count button

--- learning-session-3-react/src/App.tsx
+++ learning-session-3-react/src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
-import nerLogo from './NER-Logo-App-Icon.svg';
+import React, { useState } from "react";
+import "./App.css";
+import nerLogo from "./NER-Logo-App-Icon.svg";
+import logo from "./logo.svg";
 
 function App() {
+  const [original, setOriginal] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const onSwitchClicked = () => {
+    setOriginal(!original);
+    setCount(count+1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={nerLogo} className="App-logo" alt="logo" />
+        {original ? (
+          <img src={logo} className="App-logo" alt="logo" />
+        ) : (
+          <img src={nerLogo} className="App-logo" alt="logo" />
+        )}
         <p>NER React Learning Session</p>
         <a
           className="App-link"
@@ -16,6 +29,11 @@ function App() {
         >
           Check Out Finishline!
         </a>
+        <br></br>
+        <button onClick={onSwitchClicked} className={"App-logo-button"}>
+          Switch Logo
+        </button>
+        <p>Count: {count}</p>
       </header>
     </div>
   );
