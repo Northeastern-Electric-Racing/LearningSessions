257e0f8cd6ea6948daa850a430ed9ba8f88fd345
Carson Musser <crtmusser@gmail.com>
2022-10-03 19:51:48 -0400

    added button

--- learning-session-3-react/src/App.tsx
+++ learning-session-3-react/src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import { logDOM } from '@testing-library/react';
+import React, { useState } from 'react';
 import './App.css';
+import logo from './logo.svg';
 import nerLogo from './NER-Logo-App-Icon.svg';
 
 function App() {
+  const[original, setOriginal] = useState(false);
+
+  const onSwitchClicked = () => {
+    setOriginal(!original);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={nerLogo} className="App-logo" alt="logo" />
+        {original ? (<img src={logo} className="App-logo" alt="logo" />) :
+        (<img src={nerLogo} className="App-logo" alt="logo" />)}
         <p>NER React Learning Session</p>
         <a
           className="App-link"
@@ -16,6 +25,9 @@ function App() {
         >
           Check Out Finishline!
         </a>
+        <button onClick={onSwitchClicked} className="App-logo-button">
+        {"test"}
+        </button>
       </header>
     </div>
   );
