d91205511d7bd7c39ef2e22231744b963bab8a1a
willemshak <shak.w@northeastern.edu>
2022-10-03 20:15:47 -0400

    willem shak learnign session

--- learning-session-3-react/src/App.tsx
+++ learning-session-3-react/src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import './App.css';
-import nerLogo from './NER-Logo-App-Icon.svg';
+import React, { useState } from "react";
+import "./App.css";
+import nerLogo from "./NER-Logo-App-Icon.svg";
+import logo from "./logo.svg";
+
+
 
 function App() {
+  const [original, setOriginal] = useState(false);
+  const [num, setNum] = useState(0);
+  const onSwitchClicked = () => {
+    setOriginal(!original);
+  };
+
+  const increment = () => {
+    setNum(num + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={nerLogo} className="App-logo" alt="logo" />
+        <h1> Willem Shak</h1>
+        {original ? (
+          <img src={logo} className="App-logo" alt="logo" />
+        ) : (
+          <img src={nerLogo} className="App-logo" alt="logo" />
+        )}
         <p>NER React Learning Session</p>
         <a
           className="App-link"
@@ -16,6 +34,11 @@ function App() {
         >
           Check Out Finishline!
         </a>
+        <button onClick={onSwitchClicked} className={"App-logo-button"}>
+          Switch Logo
+        </button>
+        <button onClick={increment} className={"Increment-button"}>Increment</button>
+        <body>{num}</body>
       </header>
     </div>
   );
