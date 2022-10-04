916a5fb0d9189ce38dfef7cf47da32477f2e9d7c
thomaskummer1 <twk23@outlook.com>
2022-10-03 20:14:41 -0400

    added button

--- learning-session-3-react/src/App.tsx
+++ learning-session-3-react/src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import nerLogo from './NER-Logo-App-Icon.svg';
+import logo from "./logo.svg";
+
+
 
 function App() {
+
+  const [original, setOriginal] = useState(false);
+  const [num, setNum] = useState(0);
+
+  const onSwitchClicked = () => {
+    setOriginal(!original);
+  };
+  const onIncrementClick = () => {
+    setNum(num+1);
+   
+  }
   return (
+  
     <div className="App">
       <header className="App-header">
-        <img src={nerLogo} className="App-logo" alt="logo" />
+        <h1>Thomas Kummer</h1>
+      {original ? (
+          <img src={logo} className="App-logo" alt="logo" />
+        ) : (
+          <img src={nerLogo} className="App-logo" alt="logo" />
+        )}
         <p>NER React Learning Session</p>
         <a
           className="App-link"
@@ -16,6 +36,11 @@ function App() {
         >
           Check Out Finishline!
         </a>
+        <button onClick={onSwitchClicked} className={"App-logo-button"}>
+          Switch Logo
+        </button>
+        <button onClick={onIncrementClick}>Increment</button>
+        <p>{num}</p>
       </header>
     </div>
   );
