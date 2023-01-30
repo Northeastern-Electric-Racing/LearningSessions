1ab1664d447fc1bef7fff5a5106afdd230a75802
anthonybernardi <anthony.bernardi09@gmail.com>
2023-01-30 11:37:28 -0500

    typescript exercises

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,66 @@
-  console.log('ab')
+// NOTE: GIVE EVERYTHING TYPE ANNOTATIONS!!!!!
+
+/**
+ * #0: cd into this folder and run `yarn install`
+ *     whenever you need to run this file, run `yarn start`
+ */
+
+/**
+ * #1: write a console log that says hello world. then run yarn start. it should show up
+ *     after each exercise, you should console log stuff to make sure you did it right
+ */
+
+/**
+ * #2: declare a constant variable named `me` that has a type of `string` and has the value of your name
+ */
+
+/**
+ * #3: declare a variable named `z` that has type `number` and has the value `4`
+ *     then on the next line, reassign it to the value `3`.
+ *     How does this differ from #2?
+ */
+
+/**
+ * #4: declare a constant `fruits` that is a list of strings. Make its contents your 10 favorite fruits
+ */
+
+/**
+ * #5: write an arrow function `hasFavorite` that takes in a list of strings `ls` and returns a boolean
+ *     this function should return true if the list includes the string of your favorite fruit and false otherwise
+ *     test it out! (Hint: google javascript list includes)
+ */
+
+/**
+ * #6: create a constant JSON object that has the following properties: `name: string;` `age: number` `born: Date` `favoriteNumbers: number[]`
+ *     fill it out with your information!
+ */
+
+/**
+ * #7: write an interface called `Animal` that has the fields: `species: string` `name: string` `age: number` `isCute: boolean`
+ */
+
+/**
+ * #8: make a list of `Animal`s and give it at least 5 different items for examples.
+ *     We will use it for the next few exercises so make sure you have some good cases!
+ */
+
+/**
+ * #9: write an arrow function that takes an input of Animal[] and return a list of the species of each given animal in a list of string
+ *     (hint: use map)
+ */
+
+/**
+ * #10: write a regular function that takes an input of Animal[] and a string `name`
+ *      this function should return true if any of the animals in that list have the given name
+ *      (hint: use `some` https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some or `find` or `findIndexOf`)
+ */
+
+/**
+ * #11: write a function that takes in an input of Animal[] and a number `minAge`
+ *      this function should return a list of Animals that are older than the `minAge`
+ *      (hint: use `filter` https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter)
+ */
+
+/**
+ * BONUS: write your own implementation of `map` and call it `myMap`. call it on one of your lists to see if it works
+ */
