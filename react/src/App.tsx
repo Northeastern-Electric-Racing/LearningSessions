80ce16df544eac57b1d136afb3db0652ee0d8730
RChandler234 <29521172+RChandler234@users.noreply.github.com>
2023-09-25 01:17:00 -0400

    starter-code-2023

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,35 +1,16 @@
-import React, { useState } from "react";
-import "./App.css";
-import nerLogo from "./NER-Logo-App-Icon.svg";
-import logo from "./logo.svg";
+import React from 'react';
+import './App.css';
+import nerLogo from './NER-Logo-App-Icon.svg';
 
 function App() {
-  const [original, setOriginal] = useState(false);
-
-  const onSwitchClicked = () => {
-    setOriginal(!original);
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        {original ? (
-          <img src={logo} className="App-logo" alt="logo" />
-        ) : (
-          <img src={nerLogo} className="App-logo" alt="logo" />
-        )}
+        <img src={nerLogo} className="App-logo" alt="logo" />
         <p>NER React Learning Session</p>
-        <a
-          className="App-link"
-          href="https://finishlinebyner.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://finishlinebyner.com" target="_blank" rel="noopener noreferrer">
           Check Out Finishline!
         </a>
-        <button onClick={onSwitchClicked} className={"App-logo-button"}>
-          Switch Logo
-        </button>
       </header>
     </div>
   );
